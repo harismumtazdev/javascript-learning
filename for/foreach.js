d3fd0bbe9f41bba597a77d1d5b63ff747db823c7
harismumtazdev <harismumtaz.dev@gmail.com>
2026-09-21 17:57:32 +0500

    21sep

---
+++ for/foreach.js
@@ -0,0 +1,12 @@
+const fruits = ["apple", "banana", "orange"];
+
+// Loop through each value in the array
+fruits.forEach(function(fruit) {
+  console.log(fruit);
+});
+const fruit = ["apple", "banana", "orange"];
+
+// Loop through each value in the array
+fruits.forEach(function(fruitss) {
+  console.log(fruitss);
+});
