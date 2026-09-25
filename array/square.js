9ca9f4d6734e52933eb4cb9bc5c2bb9692f53095
harismumtazdev <harismumtaz.dev@gmail.com>
2026-09-25 20:08:04 +0500

    25sep

---
+++ array/square.js
@@ -0,0 +1,11 @@
+let arr=["ali",4,"muneeb",7,"areeb",5];
+let array=[12,12,1,214,21,34,45,65,67,78]
+for(let i=0;i<array.length;i++){
+    if(i==4){
+        let result= array[i]*array[i]
+        console.log(result);
+        
+    }
+}
+
+
